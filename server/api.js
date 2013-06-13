d8a0a7a8cdd081118c06d99cacf45325ad5d717e
Moshe Holtzberg <moholtzberg@gmail.com>
2013-06-13 17:00:03 -0400

    disabled collectionApi

--- server/api.js
+++ server/api.js
@@ -1,25 +1,17 @@
-Meteor.startup(function () {
-	
-	 collectionApi = new CollectionAPI({
-		authToken: undefined,              // Require this string to be passed in on each request
-		apiPath: 'collectionapi',          // API path prefix
-		standAlone: false,                 // Run as a stand-alone HTTP(S) server
-		sslEnabled: false,                 // Disable/Enable SSL (stand-alone only)
-		listenPort: 3005,                  // Port to listen to (stand-alone only)
-		listenHost: undefined,             // Host to bind to (stand-alone only)
-		privateKeyFile: 'privatekey.pem',  // SSL private key file (only used if SSL is enabled)
-		certificateFile: 'certificate.pem' // SSL certificate key file (only used if SSL is enabled)
-	});
-	
-	collectionApi.addCollection(Messages, 'messages', {
-		authToken: undefined, 
-		methods: ['POST'],
-		before: {
-			POST: undefined
-		}
-	});
-	
-// Starts the API server
-collectionApi.start();
-
-});
+// Meteor.startup(function () {
+// 	
+// 	collectionApi = new CollectionAPI({
+// 		authToken: undefined,              // Require this string to be passed in on each request
+// 		apiPath: 'collectionapi',          // API path prefix
+// 		standAlone: false,                 // Run as a stand-alone HTTP(S) server
+// 		sslEnabled: false,                 // Disable/Enable SSL (stand-alone only)
+// 		listenPort: 3005,                  // Port to listen to (stand-alone only)
+// 		listenHost: undefined,             // Host to bind to (stand-alone only)
+// 		privateKeyFile: 'privatekey.pem',  // SSL private key file (only used if SSL is enabled)
+// 		certificateFile: 'certificate.pem' // SSL certificate key file (only used if SSL is enabled)
+// 	});
+// 	
+// // Starts the API server
+// collectionApi.start();
+// 
+// });
--- server/messages.js
+++ server/messages.js
@@ -12,6 +12,14 @@ Meteor.startup(function () {
 			body: "Test", 
 			html: "Test"});
 	};
+
+	// collectionApi.addCollection(Messages, 'messages', {
+	// 	authToken: undefined, 
+	// 	methods: ['POST'],
+	// 	before: {
+	// 		POST: undefined
+	// 	}
+	// });
 	
 });
 
