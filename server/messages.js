206b8e08d1919a6039f22214c36a996552ff14e9
Moshe Holtzberg <moholtzberg@gmail.com>
2013-06-13 01:31:28 -0400

    added api rm mail_url

--- server/messages.js
+++ server/messages.js
@@ -2,19 +2,26 @@
 
 Messages = new Meteor.Collection("messages");
 	
-// Meteor.startup(function () {
-// 	
-// 	if (Messages.find({}).count() === 0) {
-// 		Messages.insert({
-// 			to: Meteor.user().emails[0].address, 
-// 			from: Meteor.user().emails[0].address, 
-// 			subject: "Test", 
-// 			body: "Test", 
-// 			html: "Test"});
-// 	};
-// 
-// });
+Meteor.startup(function () {
+	
+	if (Messages.find({}).count() === 0) {
+		Messages.insert({
+			to: Meteor.user().emails[0].address, 
+			from: Meteor.user().emails[0].address, 
+			subject: "Test", 
+			body: "Test", 
+			html: "Test"});
+	};
 
+	collectionApi.addCollection(Messages, 'messages', {
+		authToken: undefined, 
+		methods: ['POST'],
+		before: {
+			POST: undefined
+		}
+	});
+	
+});
 
 Messages.allow({
 	insert: function() {
@@ -27,21 +34,32 @@ Meteor.publish("Messages", function () {
 });
 
 Meteor.methods({
-  sendEmail: function (to, from, subject, text, html) {
-    check([to, from, subject, text], [String]);
-		
-		this.unblock();
-		
-		if (Email.send({
-			to: to,
-			from: from,
-			subject: subject,
-			text: text,
-			html: html
-			})) {
-				Emails.insert({to: to, from: from, subject: subject, text: text, html: html,sent_at: Time.now()})
-				};
-		console.log("sentFromHere");
-		return true;
-  }
+  // sendEmail: function (to, from, subject, text, html) {
+  //       check([to, from, subject, text], [String]);
+  //   		
+  //   		this.unblock();
+  //   		
+  //   		var msg_id = Messages.insert({user_id: this.userId, to: to, from: from, subject: subject, text: text, html: html,sent_at: new Date()});
+  // 			console.log(msg_id);
+  //   		Email.send({ "h:X-Mailgun-Variables": {"tracking_id": msg_id},"to": to, "from": from, "subject": subject, "text": text, "html": html});
+  //   		
+  //   		return true;
+  //   	}
+	
+	sendEmail: function(to, from, subject, text, html) {
+		// var msg_id = Messages.insert({user_id: this.userId, to: to, from: from, subject: subject, text: text, html: html,sent_at: new Date()});
+		Meteor.http.post("https://api.mailgun.net/v2/moholtzberg.mailgun.org/messages",
+			{auth:"api:" + "key-88943ko5dq95izwzge8xxe7kn5-s9ka4",
+			params: {
+				"from": from,
+				"to": to,
+				"subject": subject,
+				"html": html,
+				"h:X-Mailgun-Variables": JSON.stringify({user_id:this.userId}),
+				"o:tracking": "True"
+				}
+			}, function(error, result){
+		});
+		console.log("line 60");
+	}
 });
